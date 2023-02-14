fc4c0a92790ba44d26cd5bba071bac9f82e5e6d6
Lucas F Ramos <drawingworldab@gmail.com>
2023-02-14 20:38:28 -0300

    assets base do jogo

---
+++ Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public Transform Player;
+    public Rigidbody2D rig;
+
+    void Start()
+    {
+        transform.localEulerAngles = Player.transform.localEulerAngles;
+        transform.position = Player.transform.position;
+    }
+
+
+    void Update()
+    {
+        transform.localPosition = transform.localPosition + Vector3.up * speed;
+        
+    }
+}
---
+++ player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed;
+    public GameObject bullet;
+    public Transform Gun;
+    public float rotationX;
+    public float rotationY;
+
+    public static player controller;
+    void Start()
+    {
+        controller = this;
+        Cursor.visible = false;
+    }
+
+    
+    void Update()
+    {
+        Vector3 moviment = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        transform.position = transform.position + moviment * speed * Time.deltaTime;
+
+
+
+        Vector3 mousePos = Input.mousePosition * 30;
+        mousePos = Camera.main.ScreenToViewportPoint(mousePos);
+
+
+       
+
+       
+
+       
+
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x , mousePos.y - transform.position.y );
+        transform.up = direction;
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject NewBallet = Instantiate(bullet);
+        }
+    }
+}
